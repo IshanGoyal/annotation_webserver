b0a8a865cc75ae2220a5eb4a35525e2a856fbd58
acrinklaw <austincrinklaw@gmail.com>
2018-05-17 15:50:59 -0700

    Initial Django structure and model

---
+++ annotation_server/annotation_table/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Sequence(models.Model):
+    seq_name = models.CharField(max_length=200)
+    blast = models.CharField(max_length=200)
+    pfam = models.CharField(max_length=200)
+    prosite = models.CharField(max_length=200)
+    kegg = models.CharField(max_length=200)
+    go = models.CharField(max_length=200)
+    comment = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.seq_name
